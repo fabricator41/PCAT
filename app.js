82e055011a5479b4a8d9354795c4aeef8dcd7656
Emre Ilhan <emrilhan@gmail.com>
2022-12-13 15:25:41 +0300

    görsel yüklemek

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose.set('strictQuery', true);
 
 // connect DB
 
@@ -18,11 +22,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // ROUTES
 
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
     photos,
   });
@@ -46,8 +51,26 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  // console.log(req.files.image);
+  // await Photo.create(req.body);
+  // res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
